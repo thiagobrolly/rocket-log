bcd381557cdbdac7ae34bdd05260211497558c5a
Thiago Brolly <thiagoex377@gmail.com>
2025-05-29 16:38:26 -0300

    feat: finalizando retorno do usuário

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,12 +1,44 @@
+import { authConfig } from '@/configs/auth';
 import { prisma } from '@/database/prisma';
 import { AppError } from '@/utils/AppError';
-import { hash } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
 import { Request, Response } from 'express';
+import { sign } from 'jsonwebtoken';
 import { z } from 'zod';
 
 class SessionsController {
-  create(request: Request, response: Response) {
-    return response.json({ message: 'ok' });
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { email, password } = bodySchema.parse(request.body);
+
+    const user = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new AppError('Invalid email or password', 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError('Invalid email or password', 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({ role: user.role ?? 'customer' }, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    const { password: hashedPassword, ...userWithoutPassword } = user;
+
+    return response.json({ token, user: userWithoutPassword });
   }
 }
 
