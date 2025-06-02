150f7ef5365d2e9125c07609d9f22d07469f4717
Thiago Brolly <thiagoex377@gmail.com>
2025-06-02 10:50:53 -0300

    feat: atualizando status da entrega

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,13 +1,17 @@
 import { DeliveriesController } from '@/controllers/deliveries-controller';
+import { DeliveriesStatusController } from '@/controllers/deliveries-status-controller';
 import { ensureAuthenticated } from '@/middlewares/ensure-authenticated';
 import { verifyUserAuthorization } from '@/middlewares/verifyUserAuthorization';
 import { Router } from 'express';
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
+const deliveriesStatusController = new DeliveriesStatusController();
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(['sale']));
 deliveriesRoutes.post('/', deliveriesController.create);
 deliveriesRoutes.get('/', deliveriesController.index);
+
+deliveriesRoutes.patch('/:id/status', deliveriesStatusController.update);
 
 export { deliveriesRoutes };
