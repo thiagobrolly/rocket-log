152ac6c9b6b74768cfbe090dce86983a74e70cac
Thiago Brolly <thiagoex377@gmail.com>
2025-06-02 16:21:25 -0300

    fix: show delivery not found

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -55,6 +55,10 @@ class DeliveryLogsController {
       },
     });
 
+    if (!delivery) {
+      return response.status(404).json({ message: 'delivery not found' });
+    }
+
     if (
       request.user?.role === 'customer' &&
       request.user.id !== delivery?.userId
